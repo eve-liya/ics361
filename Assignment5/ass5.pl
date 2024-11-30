cf356024b10cdc3374814fa9c626a8bac2af5b2c
vincent <vincenthawaii4125@gmail.com>
2024-11-29 23:33:46 -1000

    bruhda

--- Assignment5/ass5.pl
+++ Assignment5/ass5.pl
@@ -15,6 +15,7 @@ Ans = homer.
 
 */
 
+
 query(Question, _) :-
   yesnoquestion(Question, []).
 
@@ -27,18 +28,19 @@ This is a DCG (Definite Clause Grammar).
 
 */
 
+% A yes no question can be made out of multiple sentences
 yesnoquestion --> [is,it,true,that], multiple_sentences(Q), {Q}.
+
+% yesnoquestion can also be 
 yesnoquestion --> [is,it,true,that], sentence(Q), {Q}.
 
 % Multiple sentences are a sentence seperated by "and", both of them have to be true
 multiple_sentences(Q) --> sentence(Q),[and],multiple_sentences(Qs),{Q,Qs}.
 
 % A multiple sentence can be a single sentence
-multiple_sentences(Q) --> sentence(Q), {Q}.
+multiple_sentences(Q) --> sentence(Q).
 
 % base case of sentence
-sentence(Q) --> person(X), [is,a], relation(Y), {=..(Q, [Y,X,_])}.
-sentence(Q) --> person(X), [has,a], relation(Y), {=..(Q, [Y,X,_])}.
 
 sentence(Q) --> 
     person(X), 
@@ -52,30 +54,36 @@ sentence(Q) -->
     person(X), 
     [is, a], 
     relation(Rel, num=sing), 
-    {=..(Q, [Rel, X, _])}.
+    {=..(Q, [role,Rel, X, _])}.
+
+sentence(Q) --> 
+    person(X),
+    [has, a],
+    relation(Rel, num=sing), 
+    {=..(Q, [Rel, _, X])}.
 
 sentence(Q) --> 
     person(X), 
-    [has, a], 
-    relation(Rel, num=sing), 
-    {=..(Q, [Rel, X, _])}.
+    [has], 
+    relation(Rel, num=plur), 
+    {=..(Q, [Rel, _, X])}.
 
 sentence(Q) --> 
-    people(XS), 
+    people(XS, num=plur), 
     [are], 
     relation(Rel, num=plur),
     {=..(Q, [role,Rel,XS,_])}.
 
 sentence(Q) -->
-    people(XS),
+    people(XS, num=plur),
     [have],
     relation(Rel, num=plur),
-    {=..(Q, [role,Rel,XS,_])}.
+    {=..(Q, [has_a,Rel,XS,_])}.
 
 whoisquestion(Answer) --> [who, is, the], relation(X, num=sing), [of], person(Y), {=..(Q, [X,Answer,Y]), Q}.
 
-people([P | PS]) --> person(P), [and] ,people(PS).
-people([P]) --> person(P).
+people([P | PS], num=plur) --> person(P), [and], people(PS, num=_).
+people([P], num=sing) --> person(P).  
 
 person(homer) --> [homer].
 person(lisa) --> [lisa].
@@ -128,19 +136,46 @@ relation(brother, num=plur) --> [brothers].
 relation(sister, num=sing) --> [sister].
 relation(sister, num=plur) --> [sisters].
 
+% alternative terms
+relation(father, num=sing) --> [papa].
+relation(father, num=plur) --> [papas].
+relation(mother, num=sing) --> [mama].
+relation(mother, num=plur) --> [mamas].
+relation(cousin, num=sing) --> [cuz].
+relation(cousin, num=plur) --> [cuzs].
+relation(uncle, num=sing) --> [unc].
+relation(uncle, num=plur) --> [uncs].
+relation(aunt, num=sing) --> [aunty].
+relation(aunt, num=plur) --> [aunties].
+relation(brother, num=sing) --> [bro].
+relation(brother, num=plur) --> [bros].
+relation(brother, num=sing) --> [bruddah].
+relation(brother, num=plur) --> [bruddahs].
+relation(sister, num=sing) --> [sis].
+relation(sister, num=sing) --> [sistah].
+relation(sister, num=plur) --> [sistahs].
+relation(sister, num=plur) --> [sissies].
+relation(grandfather, num=sing) --> [grandpa].
+relation(grandfather, num=plur) --> [grandpas].
+relation(grandmother, num=sing) --> [grandma].
+relation(grandmother, num=plur) --> [grandmas].
+
+
 /* THE DEFINITIONS
 
 For example: X is Y's father if X is the parent of Y and X is male.
 
 */
-
-handle_plural([P1, P2 | PS], Relation, Q) :-
-    role(Relation, [P1, P2 | PS], Q).
-
 role(_,[],[]).
 
 role(Rel, [HP | RP], [HQ | RQ]) :-
     HQ =.. [Rel, HP, _],
+    role(Rel,RP,RQ).
+
+has_a(_,[],[]).
+
+has_a(Rel, [HP | RP], [HQ | RQ]) :-
+    HQ =.. [Rel, _, HP],
     role(Rel,RP,RQ).
 
 father(X,Y) :-
@@ -257,12 +292,13 @@ ancestor(X,Y) :-
     parent(Parent, Y),
     ancestor(X, Parent).
 
-descendant(X,Y) :-
-    parent(Y,X).
+% A descendent is a child or a descendant of a child.
+descendant(Descendant, X) :-
+  child(Descendant, X).
 
-descendant(X,Y) :-
-    parent(Y, Child),
-    descendant(Child, Y).
+descendant(Descendant, X) :-
+  child(Child, X),
+  descendant(Descendant, Child).
 
 /* THE FACTS
 
